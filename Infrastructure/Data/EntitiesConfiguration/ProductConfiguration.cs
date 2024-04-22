e9ce6b1d2402b89c9f49ce48349de24a19ee9422
Kareem Karam <subinsight@hotmail.com>
2024-04-22 19:21:00 +0200

    Commiting Initial Project

---
+++ Infrastructure/Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration
+    {
+        public ProductConfiguration(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+            builder.HasKey(p => p.Id).HasName("PK_Product_Id");
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Infrastructure.Data.EntitiesConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new ProductConfiguration(modelBuilder.Entity<Product>());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
